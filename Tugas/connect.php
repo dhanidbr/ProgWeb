8445d6b6a3f11300cf60077d3ae1ca1c20eb8bf8
dhanidbr <60741171+dhanidbr@users.noreply.github.com>
2021-05-04 00:06:14 +0700

    Delete Tugas directory

--- Tugas/connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-$con = mysqli_connect("localhost", "root", "", "loginphp");
-
-// Check connection
-if (mysqli_connect_errno()) {
-    echo "Koneksi database gagal : " . mysqli_connect_error();
-}
